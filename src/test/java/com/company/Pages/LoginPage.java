391d92a8d291f3f22289a602943995c8b42952fa
yarik <yariks@i.ua>
2018-02-04 00:05:49 +0200

    allure

--- src/test/java/com/company/Pages/LoginPage.java
+++ src/test/java/com/company/Pages/LoginPage.java
@@ -2,14 +2,19 @@ package com.company.Pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class LoginPage extends BasePage {
 
-	@FindBy(name="login")
-	private WebElement inputLogin;
+    @FindBy(name = "login")
+    private WebElement inputLogin;
 
     @FindBy(name = "pass")
     private WebElement inputPassword;
+
+    @FindBy(name = "domn")
+    private WebElement domen;
 
     @FindBy(xpath = "//form[@name='lform']//input[@type='submit']")
     private WebElement btnSubmit;
@@ -18,7 +23,7 @@ public class LoginPage extends BasePage {
     public void login(String name, String password) {
         inputLogin.sendKeys(name);
         inputPassword.sendKeys(password);
-         btnSubmit.click();
+        btnSubmit.click();
 
     }
 }
--- src/test/java/com/company/Tests/MyTest.java
+++ src/test/java/com/company/Tests/MyTest.java
@@ -3,13 +3,15 @@ package com.company.Tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
 
 import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
-
+@Description("This is an example test suite")
 public class MyTest extends BaseTest {
-
+    @Title("Successful test")
     @Test
     public void testMailLogin() throws AWTException {
         WebDriver driver = app.common.getDriver();
