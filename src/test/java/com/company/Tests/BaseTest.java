ecbf656cb3d0947ce041aeeb0bfeabf657a00011
yarik <yariks@i.ua>
2018-02-04 08:48:51 +0200

    allure

--- src/test/java/com/company/Tests/BaseTest.java
+++ src/test/java/com/company/Tests/BaseTest.java
@@ -1,13 +1,15 @@
 package com.company.Tests;
 
 import com.company.App;
+import com.company.ScreenShotOnFailListener;
 import com.company.Users.User;
 import com.company.Users.UserFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
 import java.util.List;
-
+@Listeners(ScreenShotOnFailListener.class)
 public class BaseTest {
     protected static User validUser = UserFactory.getValidUser();
     protected static App app= new App();
